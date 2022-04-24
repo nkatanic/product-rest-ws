14a6c626f90a1fd2c5660590a2746ec3e189cf5b
Nenad Katanic <nenad.katanic@fer.hr>
2022-04-24 17:40:36 +0200

    Initial Spring-Boot project structure

---
+++ src/main/java/com/example/productrestws/ProductRestWsApplication.java
@@ -0,0 +1,13 @@
+package com.example.productrestws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRestWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/productrestws/ProductRestWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productrestws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRestWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
