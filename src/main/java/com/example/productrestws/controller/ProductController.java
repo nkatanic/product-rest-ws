18e12a6b9a2329e2791b69ffe518f64f14a061aa
Nenad Katanic <nenad.katanic@fer.hr>
2022-04-29 15:46:39 +0200

    Add API support for Update and Delete

--- src/main/java/com/example/productrestws/controller/ProductController.java
+++ src/main/java/com/example/productrestws/controller/ProductController.java
@@ -9,9 +9,11 @@ import com.example.productrestws.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,17 @@ public class ProductController {
     
     @Autowired
     ProductRepository productRepository;
+
+    @PostMapping("/products")
+	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+		try {
+			Product newProduct = productRepository
+					.save(new Product(product.getCode(), product.getName(), product.getPriceHrk(), product.getDescription(), product.getIsAvailable()));
+			return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getAllProducts() {
@@ -49,15 +62,54 @@ public class ProductController {
 		}
 	}
 
-
-    @PostMapping("/products")
-	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+	@GetMapping("/products/isAvailable")
+	public ResponseEntity<List<Product>> findByIsAvailable() {
 		try {
-			Product _product = productRepository
-					.save(new Product(product.getCode(), product.getName(), product.getPriceHrk(), product.getDescription(), product.getIsAvailable()));
-			return new ResponseEntity<>(_product, HttpStatus.CREATED);
+			List<Product> products = productRepository.findByIsAvailable(true);
+			if (products.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(products, HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
+	@PutMapping("/products/{id}")
+	public ResponseEntity<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
+		Optional<Product> productData = productRepository.findById(id);
+		if (productData.isPresent()) {
+			Product updatedProduct = productData.get();
+			updatedProduct.setCode(product.getCode());
+			updatedProduct.setName(product.getName());
+			updatedProduct.setPriceHrk(product.getPriceHrk());
+			updatedProduct.setDescription(product.getDescription());
+			updatedProduct.setIsAvailable(product.getIsAvailable());
+	
+			return new ResponseEntity<>(productRepository.save(updatedProduct), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<HttpStatus> deleteProduct(@PathVariable("id") long id) {
+		try {
+			productRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@DeleteMapping("/products")
+	public ResponseEntity<HttpStatus> deleteAllProducts() {
+		try {
+			productRepository.deleteAll();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 }
